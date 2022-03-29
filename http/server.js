ce1b60bd42881a4cff61282fb0e152fca4738e68
Bruno Coutinho <xxbrunomassaxx@gmail.com>
2022-03-29 01:55:56 -0300

    add api

--- http/server.js
+++ http/server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const http = require('http')
 const fs = require('fs')
 const path = require('path')
@@ -23,30 +22,4 @@ http.createServer((req, res) => {
         }
     )
         
-=======
-const http = require('http')
-const fs = require('fs')
-const path = require('path')
-
-http.createServer((req, res) => {
-
-    const file = req.url === '/' ? 'index.html' : req.url
-    const filePath = path.join(__dirname, 'public', file)
-    const extname = path.extname(filePath)
-
-    const allowedFileTypes = ['.html', '.css', '.js']
-    const allowed = allowedFileTypes.find(item => item == extname)
-
-    if(!allowed) return
-
-    fs.readFile(
-        filePath,
-        (err, content) => {
-            if(err) throw err
-
-            res.end(content)
-        }
-    )
-        
->>>>>>> c86df640d6e5e495d2bd19fb0c9a0bbc047150c1
 }).listen(5000, () => console.log('Server is running'))
